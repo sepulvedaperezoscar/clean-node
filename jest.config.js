1b39d5054a6938b4103a3e84c2f427f2659cee2f
Oscar Sepulveda Perez <sepulvedaperezoscar@gmail.com>
2025-11-05 19:26:17 -0500

    Add config for tests

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,30 @@
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/tests'],
-    testMatch: ['**\/*.test.ts'],
+    testMatch: ['**/*.test.ts'],
     collectCoverageFrom: [
-        'src/**\/*.ts',
-        '!src/**\/*.d.ts',
+        'src/**/*.ts',
+        '!src/**/*.d.ts',
         '!src/application/server.ts',
+        '!src/**/index.ts',
     ],
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov', 'html'],
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70,
+        },
+    },
     moduleNameMapper: {
         '^@domain/(.*)$': '<rootDir>/src/domain/$1',
         '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
         '^@application/(.*)$': '<rootDir>/src/application/$1',
     },
     setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+    verbose: true,
 };
